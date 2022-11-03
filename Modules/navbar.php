8d3ce4a288729fe4522e99bbe5203f6b97d919a0
Brendan Ball <brendan.ball@uleth.ca>
2022-11-03 13:43:48 -0600

    added in active class logic in navbar module

--- Modules/navbar.php
+++ Modules/navbar.php
@@ -1,3 +1,5 @@
+<?php $page = substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'], "/") + 1); ?>
+
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php">Group 2's Computer Repair Store</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,18 +8,10 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="customer.php">Customer <span class="sr-only">(current)</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="stock.php">Stock <span class="sr-only">(current)</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="invoice.php">Invoice <span class="sr-only">(current)</a>
-      </li>
+      <li class="nav-item"><a class="nav-link <?= $page == 'index.php' ? 'active' : '' ?>" href="index.php">Home</a></li>
+      <li class="nav-item"><a class="nav-link <?= $page == 'customer.php' ? 'active' : '' ?>" href="customer.php">Customer</a></li>
+      <li class="nav-item"><a class="nav-link <?= $page == 'stock.php' ? 'active' : '' ?>" href="stock.php">Stock</a></li>
+      <li class="nav-item"><a class="nav-link <?= $page == 'invoice.php' ? 'active' : '' ?>" href="invoice.php">Invoice</a></li>
     </ul>
   </div>
 </nav>
