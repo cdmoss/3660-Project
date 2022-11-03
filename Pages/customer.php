f19a4a88622bcc1e0728cfe3d80128db52c799d5
Brendan Ball <brendan.ball@uleth.ca>
2022-11-03 11:43:37 -0600

    Moved required bootstrap files to modules folder

--- Pages/customer.php
+++ Pages/customer.php
@@ -3,26 +3,8 @@
   <head>
   </head>
   <body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="index.php">Group 2's Computer Repair Store</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="customer.php">Customer</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="stock.php">Stock</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <?php include "../Modules/navbar.php" ?>
 
   </body>
 </html>
