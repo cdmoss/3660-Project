f5508da5d9307ebb8a408be44fed052a077a7278
Brendan Ball <brendan.ball@uleth.ca>
2022-11-03 12:20:28 -0600

    Added in bare bones for each page and added in tables for stock and customer

---
+++ Pages/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  </head>
+  <body>
+
+    <!-- Navbar files -->
+    <?php include "../Modules/navbar.php" ?>
+
+  </body>
+</html>
+
+<!-- Bootstrap files -->
+<?php include "../Modules/bootstrap.php" ?>
