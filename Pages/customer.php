6593cfbf6102508c12a4950c72b118bdff9c7505
Brendan Ball <brendan.ball@uleth.ca>
2022-11-06 15:58:36 -0700

    Formatted code better, added in customerbyid page

--- Pages/customer.php
+++ Pages/customer.php
@@ -11,35 +11,47 @@
 
     <div class="container-fluid">
       <!-- Table -->
-      <table class="table mt-5">
-      <thead class="thead-dark">
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Address</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $db = Db::getInstance();
-          $result = $db->getAll('customers');
+      <form method="POST">
+        <table class="table mt-5">
+        <thead class="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Address</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $db = Db::getInstance();
+            $result = $db->getAll('customers');
 
-          if (count($result->errors) > 0) {
-            foreach ($result->errors as $error) {
-              echo "$error";
+            if (count($result->errors) > 0) {
+              foreach ($result->errors as $error) {
+                echo "$error";
+              }
             }
-          }
-          else {
-            while ($customer = $result->data->fetch()) {
-              echo "<tr><td>" . $customer['id'] . "</td><td>" . $customer['name'] . "</td><td>" . $customer['email'] . "</td><td>" . $customer['phone'] . "</td><td>" . $customer['address'] . "</td><td><button type='button' class='btn btn-primary'>View</button><button type='button' class='btn btn-secondary'>Edit</button><button type='button' class='btn btn-danger'>Delete</button></td></tr>";
+            else {
+              while ($customer = $result->data->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $customer['id'] ."</td>";
+                echo "<td>" . $customer['name'] . "</td>";
+                echo "<td>" . $customer['email'] . "</td>";
+                echo "<td>" . $customer['phone'] . "</td>";
+                echo "<td>" . $customer['address'] . "</td>";
+                echo "<td><div class='btn-group' role='group'>";
+                echo "<a href='customerbyid.php?cus_id=" . $customer['id'] . "' class='btn btn-primary'>View/Edit</a>";
+                echo "<a href='#' class='btn btn-danger'>Delete</a></td>";
+                echo "</tr>";
+                echo "</div>";
+              }
             }
-          }
-        ?>
-      </tbody>
-      </table>
+          ?>
+        </tbody>
+        </table>
+      </form>
     </div>
 
   </body>
---
+++ Pages/customerbyid.php
@@ -0,0 +1,48 @@
+<?php include "../data/db.php" ?>
+
+<?php 
+    if(isset($_POST['delCustomer'])) {
+        $db = Db::getInstance();
+        $result = $db->deleteCustomer($_GET['cus_id']);
+    }
+
+    if(isset($_POST['editCustomer'])) {
+        $db = Db::getInstance();
+        $result = $db->editCustomer
+        ($_POST['cusid'], $_POST['cusname'], $_POST['cusemail'], $_POST['cusphone'], $_POST['cusaddress']);
+    }
+?>
+
+<!-- Sidebar -->
+<?php include "../Modules/sidebar.php" ?>
+
+<?php 
+$db = Db::getInstance();
+$result = $db->getSingleById('customers', $_GET['cus_id']);
+
+if (count($result->errors) > 0) {
+    foreach ($result->errors as $error) {
+        echo "$error";
+    }
+}
+else {
+    while ($customer = $result->data->fetch()) {
+        echo "<form method='POST'>";
+        echo "<div>";
+        echo "<div class='form-group' style='min-width: 300px; width: 25%; margin-left: 2%;'><label for='cusid'>Customer ID</label><input type='text' class='form-control' id='cusid' value='" . $customer['id'] . "' readonly /></div>";
+        echo "<div class='form-group' style='min-width: 300px; width: 25%; margin-left: 2%;'><label for='cusname'>Name</label><input type='text' class='form-control' id='cusname' value='" . $customer['name'] . "' /></div>";
+        echo "<div class='form-group' style='min-width: 300px; width: 25%; margin-left: 2%;'><label for='cusphone'>Phone Number</label><input type='text' class='form-control' id='cusphone' value='" . $customer['phone'] . "' /></div>";
+        echo "<div class='form-group' style='min-width: 300px; width: 25%; margin-left: 2%;'><label for='cusemail'>Email Address</label><input type='text' class='form-control' id='cusemail' value='" . $customer['email'] . "' /></div>";
+        echo "<div class='form-group' style='min-width: 300px; width: 25%; margin-left: 2%;'><label for='cusaddress'>Address</label><input type='text' class='form-control' id='cusaddress' value='" . $customer['address'] . "' /></div>";
+        echo "<div class='btn-group' role='group' style='min-width: 300px; width: 25%; margin-left: 2%;'>";
+        echo "<input type='submit' name='editCustomer' class='btn btn-warning' value='Save Changes' />";
+        echo "<input type='submit' name='delCustomer' class='btn btn-danger' value='Delete' />";
+        echo "<a href='customer.php' class='btn btn-primary'>Go Back</a>";
+        echo "</div>";
+        echo "</div>";
+        echo "</form>";
+    }
+}
+?>
+<?php include "../Modules/linksandscripts.php" ?>
+
