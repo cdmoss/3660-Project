7c920400017a01af9d72580814a2b66f494f0e0a
brendanball4 <ball.brendan10@gmail.com>
2022-11-18 20:27:34 -0700

    Styled and changed login.php

--- Pages/login.php
+++ Pages/login.php
@@ -16,16 +16,15 @@ if (isset($_POST['submitLogin'])) {
 </head>
 
 <body id="page-top">
-  <?php include "../Modules/sidebar.php" ?>
-
-  <div class="form-group login">
-    <h1 style="text-align:center;">Login</h1>
+  <div class="form-group login center-screen">
+  ` <img src="../images/logoblack.png" style="margin-left: 15%; margin-bottom: -50px;" alt="Group 2 Computer Repair logo" width="300" height="300"> 
+    <h1 style="text-align:center;">Computer Repair Login</h1>
     <hr>
     <form method='POST'>
-      <div class='form-group'><label for='loginUser'>Username</label><input type='text' class='form-control' name='loginUser' /></div>
-      <div class='form-group'><label for='loginPass'>Password</label><input type='text' class='form-control' name='loginPass' /></div>
+      <div class='form-group'><label for='loginUser'>Username:</label><input type='text' class='form-control' name='loginUser' /></div>
+      <div class='form-group'><label for='loginPass'>Password:</label><input type='text' class='form-control' name='loginPass' /></div>
       <hr>
-      <input type='submit' name='submitLogin' class='btn btn-primary' value='Login' />
+      <input type='submit' style='width: 100%' name='submitLogin' class='btn btn-primary' value='Login' />
     </div>
   </form>
 </div>
