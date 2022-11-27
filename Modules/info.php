e7e894683ed299cbee7bf78e325bc343c9e2f04e
brendanball4 <ball.brendan10@gmail.com>
2022-11-27 16:56:46 -0700

    Changed success margins

--- Modules/info.php
+++ Modules/info.php
@@ -1,5 +1,5 @@
 <?php
-echo "<div class='border-left-success alert alert-success alert-dismissible fade mt-3 show' role='alert'>";
+echo "<div class='border-left-success alert alert-success alert-dismissible fade mt-3 mr-3 ml-3 show' role='alert'>";
         echo $_SESSION['alertmessage'];
         echo "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
           echo "<span aria-hidden='true'>&times;</span>";
